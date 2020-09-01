086f69ed8c0ee38f0e488a718152f619cde30cfd
OmarShaker97 <mrnickgage@gmail.com>
2020-09-01 15:56:46 +0200

    Feat: added basic css

--- resources/views/userinformation/generatepdf.blade.php
+++ resources/views/userinformation/generatepdf.blade.php
@@ -1,10 +1,25 @@
+<head>
+    <link rel="stylesheet" href="{{ URL::asset('css/userinfo.css') }}">
+</head>
+
 <form method="POST" action="generatepdf">
     <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
     <table>
+        
         <tr>
-        <td>Insert User ID</td>
-        <td><input type='text' name='id' /></td>
-        <td><input type = 'submit' value = "Submit"/></td>
+            <td>
+            <div class="group">      
+              <input type="text" name='id' required>
+              <span class="highlight"></span>
+              <span class="bar"></span>
+              <label>
+                Insert User ID
+              </label>
+            </div>
+            </td>
+          </tr>
+        <tr>
+        <td><input type = 'submit' value = "Submit" class="btn"/></td>
         </tr> 
     </table>
 </form>
--- resources/views/userinformation/insert.blade.php
+++ resources/views/userinformation/insert.blade.php
@@ -1,37 +1,108 @@
+
+<head>
+  <link rel="stylesheet" href="{{ URL::asset('css/userinfo.css') }}">
+</head>
+
+
 <form method="POST" action="{{ route('userinfo.store')}}">
     <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
-    <table>
+    <table class="table">
+
         <tr>
-          <td><input type='text' name='name' /></td>
-        <td>إسم صاحب
-          الحساب</td>
+          <td>
+          <div class="group">      
+            <input type="text" name='name' required>
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>
+              إسم صاحب
+                الحساب
+            </label>
+          </div>
+          </td>
         </tr>
+
         <tr>
-        <td><input type='text' name='address' /></td>
-        <td>العنوان</td>
+          <td>
+          <div class="group">      
+            <input type="text" name='address' required>
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>
+              العنوان
+            </label>
+          </div>
+          </td>
         </tr>
-        <tr>   
-        <td><input type='text' name='country' /></td>
-        <td>دولة</td>
-        </tr>
+
         <tr>
-        <td><input type='text' name='city' /></td>
-        <td>مدينة</td>
+          <td>
+          <div class="group">      
+            <input type="text" name='country' required>
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>
+              دولة
+            </label>
+          </div>
+          </td>
         </tr>
+
         <tr>
-        <td><input type='text' name='telephone' /></td>
-        <td>تليفون</td>
+          <td>
+          <div class="group">      
+            <input type="text" name='city' required>
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>
+              مدينة
+            </label>
+          </div>
+          </td>
         </tr>
+
+        
         <tr>
-        <td><input type='text' name='fax' /></td>
-        <td>فاكس</td>
+          <td>
+          <div class="group">      
+            <input type="text" name='telephone' required>
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>
+              تليفون
+            </label>
+          </div>
+          </td>
         </tr>
+
         <tr>
-        <td><input type='text' name='email' /></td>
-        <td>بريد إلكتروني</td>
+          <td>
+          <div class="group">      
+            <input type="text" name='fax' required>
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>
+              فاكس
+            </label>
+          </div>
+          </td>
         </tr>
+
         <tr>
-      <td><input type = 'submit' value = "تسجيل"/></td>
+          <td>
+          <div class="group">      
+            <input type="text" name='email' required>
+            <span class="highlight"></span>
+            <span class="bar"></span>
+            <label>
+              بريد إلكتروني
+            </label>
+          </div>
+          </td>
+        </tr>
+
+        <tr>
+      <td><input type = 'submit' value = "تسجيل" class="btn"/></td>
     </tr>
         
     </table>
